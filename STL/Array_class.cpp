b04f9feaa6d06c9a9416d86e3230170d9c2ed7a8
sangiii1725 <bodasangeeth1997@gmail.com>
2026-01-12 10:16:19 +0530

    Array Class Update

--- STL/Array_class.cpp
+++ STL/Array_class.cpp
@@ -5,30 +5,45 @@
 
 int main(){
     
-    std::array<std::string, 8> arr;  
-    std::array<std::string, 8> arr2;
-    
+    std::array<std::string, 5> arr;
+    std::array<std::string, 5> arr2;
+    // std::array<const std::string, 5> arr3;
+
     arr.fill("sangi");
-    
-    for(int i=0; i<arr.size(); i++){
-        std::cout << arr.at(i) << std::endl;
-        std::cout << arr[i] << std::endl;
-        std::cout << std::get<0>(arr) << std::endl;
-    }
-    
+    arr2.fill("roopa");
+    // arr3.fill("roopa");
+    // if(arr3 == arr2){std::cout << "arra2 and arra3 are equal";}
+
+    std::cout << arr.size() << std::endl;
     std::cout << arr.max_size() << std::endl;
-    
-    std::cout << *(arr.data() + 3) << std::endl;
-    
+
     std::cout << arr.front() << std::endl;
     std::cout << arr.back() << std::endl;
-    std::cout << arr.empty() << std::endl;
-    
+    std::cout << *(arr.data() + 2) << std::endl;
+
     arr.swap(arr2);
-    std::cout << arr.size() << std::endl;
-    for(auto value: arr){
-        std::cout << value << std::endl;
+
+    int i=0;
+    while (!arr2.empty())
+    {
+        std::cout << arr2.at(i) << std::endl;
+        i++;
     }
+
+    for(auto it=arr2.begin(); it!=arr2.end(); it++){
+        std::cout << *it << std::endl;
+    }
+    for(auto it=arr2.rbegin(); it!=arr2.rend(); it++){
+        std::cout << *it << std::endl;
+    }
+    // for(auto it=arr3.cbegin(); it!=arr3.cend(); it++){
+    //     std::cout << *it << std::endl;
+    // }
+    // for(auto it=arr3.crbegin(); it!=arr3.crend(); it++){
+    //     std::cout << *it << std::endl;
+    // }
+
+    
     
     return 0;
 }
